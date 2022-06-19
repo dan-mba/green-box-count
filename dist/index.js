4c1cd486be209b8cc7b4e88aa61d462a11e7bcf2
GitHub Action <action@github.com>
2022-06-19 11:38:34 +0000

    Build Action

--- dist/index.js
+++ dist/index.js
@@ -116,6 +116,9 @@ async function getSquaresCount(login, myToken) {
         days.sort((a, b) => Number(b.date) - Number(a.date));
         // Don't end streak based on current day, but add it if contribution made
         let count = days.slice(1).findIndex(d => d.count === 0);
+        if (count === -1) {
+            count = days.length - 1;
+        }
         if (days[0].count > 0)
             count++;
         return count;
